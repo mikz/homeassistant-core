27254abfc65c4ea0b8690215d0175e83ea87198b
Michal Cichra <mikz@users.noreply.github.com>
2024-09-20 09:52:48 +0200

    working velux active integration

--- homeassistant/components/velux_active/const.py
+++ homeassistant/components/velux_active/const.py
@@ -1,7 +1,13 @@
 """Constants for the velux_active integration."""
 
+import logging
+
+LOGGER = logging.getLogger(__package__)
+
 DOMAIN = "velux_active"
 
-# TODO Update with your own urls
-OAUTH2_AUTHORIZE = "https://www.example.com/auth/authorize"
-OAUTH2_TOKEN = "https://www.example.com/auth/token"
+API_URL = "https://app.velux-active.com"
+OAUTH2_TOKEN = f"{API_URL}/oauth2/token"
+
+OAUTH2_CLIENT_ID = "5931426da127d981e76bdd3f"
+OAUTH2_CLIENT_SECRET = "6ae2d89d15e767ae5c56b456b452d319"
